d51346befc512952f21ff44f58602269cf88efbd
Ruk228 <icecreamcrusher21@gmail.com>
2023-07-21 16:02:32 +0400

    painted star

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import time
 import curses
 
 
+def show_addstr(row, column, canvas, time_sleep, *a_dim):
+    canvas.addstr(row, column, '*', *a_dim)
+    canvas.refresh()
+    time.sleep(time_sleep)
+
+
 def draw(canvas):
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
     canvas.border()
-    canvas.refresh()
-    time.sleep(1)
+
+    while True:
+        show_addstr(row, column, canvas, 2, curses.A_DIM)
+        show_addstr(row, column, canvas, 0.3)
+        show_addstr(row, column, canvas, 0.5, curses.A_BOLD)
+        show_addstr(row, column, canvas, 0.3)
+
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
+    curses.curs_set(False)
