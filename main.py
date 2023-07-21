e438150206b845c71d81cc7ebe515a469d656ad0
Ruk228 <icecreamcrusher21@gmail.com>
2023-07-21 19:26:11 +0400

    optimized 5 stars

--- main.py
+++ main.py
@@ -4,29 +4,45 @@ import asyncio
 
 
 async def blink(canvas, row, column, symbol='*'):
-    while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+    canvas.addstr(row, column, symbol, curses.A_DIM)
+    await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+    canvas.addstr(row, column, symbol)
+    await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+    canvas.addstr(row, column, symbol, curses.A_BOLD)
+    await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+    canvas.addstr(row, column, symbol)
+    await asyncio.sleep(0)
+
+
+def get_coroutines(canvas, row, column):
+    coroutines = []
+
+    for num in range(5):
+        column += 1
+        coroutines.append(blink(canvas, row, column))
+    return coroutines
 
 
 def draw(canvas):
-    row, column = (5, 20)
     canvas.border()
-    coroutine = blink(canvas, row, column)
+    row, column = (5, 20)
+
+    coroutines = get_coroutines(canvas, row, column)
 
     while True:
-        coroutine.send(None)
-        canvas.refresh()
+
+        for coroutine in coroutines:
+            try:
+                coroutine.send(None)
+                canvas.refresh()
+            except StopIteration:
+                coroutines.remove(coroutine)
         time.sleep(1)
+        if len(coroutines) == 0:
+            break
 
 
 if __name__ == '__main__':
