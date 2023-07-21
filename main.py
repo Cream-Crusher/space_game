02df822874fff9900d80970d9bbc1186ee748f74
Ruk228 <icecreamcrusher21@gmail.com>
2023-07-21 20:17:13 +0400

    added animation scheme

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import time
 import curses
 import asyncio
 
+TIC_TIMEOUT = 0.1
 
-async def blink(canvas, row, column, symbol='*'):
+
+async def blink(canvas, row, column, symbol='*'):  # TODO наверное нужно  убрать копипасть, кхкх.
     canvas.addstr(row, column, symbol, curses.A_DIM)
     await asyncio.sleep(0)
+    for num in range(20):
+        await asyncio.sleep(0)
 
     canvas.addstr(row, column, symbol)
     await asyncio.sleep(0)
+    for num in range(3):
+        await asyncio.sleep(0)
 
     canvas.addstr(row, column, symbol, curses.A_BOLD)
     await asyncio.sleep(0)
+    for num in range(5):
+        await asyncio.sleep(0)
 
     canvas.addstr(row, column, symbol)
     await asyncio.sleep(0)
+    for num in range(3):
+        await asyncio.sleep(0)
 
 
 def get_coroutines(canvas, row, column):
@@ -40,7 +50,7 @@ def draw(canvas):
                 canvas.refresh()
             except StopIteration:
                 coroutines.remove(coroutine)
-        time.sleep(1)
+        time.sleep(TIC_TIMEOUT)
         if len(coroutines) == 0:
             break
 
