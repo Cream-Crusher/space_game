1d3065fc5b233a881388d3cef262b1d9250a933b
Ruk228 <icecreamcrusher21@gmail.com>
2023-07-21 16:45:30 +0400

    revived the animation

--- main.py
+++ main.py
@@ -18,26 +18,15 @@ async def blink(canvas, row, column, symbol='*'):
         await asyncio.sleep(0)
 
 
-# def show_addstr(row, column, canvas, time_sleep, *a_dim):
-#     canvas.addstr(row, column, '*', *a_dim)
-#     canvas.refresh()
-#     time.sleep(time_sleep)
-
-
 def draw(canvas):
     row, column = (5, 20)
     canvas.border()
     coroutine = blink(canvas, row, column)
-    coroutine.send(None)
-    canvas.refresh()
-    time.sleep(1)
 
-    # while True:
-    #     show_addstr(row, column, canvas, 2, curses.A_DIM)
-    #     show_addstr(row, column, canvas, 0.3)
-    #     show_addstr(row, column, canvas, 0.5, curses.A_BOLD)
-    #     show_addstr(row, column, canvas, 0.3)
-
+    while True:
+        coroutine.send(None)
+        canvas.refresh()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
